1e000231981fec88663c577cdecf6a25ba3c1f76
Librazionpc <nwajeizion393@gmail.com>
2023-08-18 17:25:22 +0100

    Dealing with characters %c

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 int _printf(const char *format, ...);
-void print_string(char *s);
-void print_char(char c);
+int print_string(char *s);
+int print_char(char c);
 #endif
