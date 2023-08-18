82d0819ba628a67f12fce2e8dc4cb639201a9976
PcNerd <ajayihabeeb977@gmail.com>
2023-08-18 18:20:33 +0100

    dealing with git issue

--- character_functions.c
+++ character_functions.c
@@ -1,16 +1,34 @@
 #include <unistd.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdarg.h>
+
 
 /**
+ * print_char - print character
+ * @c: the character to be printed
  *
- *
- *
- *
+ *Return: 0 on success
  */
 
 int print_char(char c)
 {
 	write(1, &c, 1);
-	return(0);
+	return (0);
+}
+/**
+ * print_string - print string using write function
+ * @s: a pointer to the string to print
+ *
+ * Return: 0 on success
+ */
+
+int print_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		write(1, &s[i], 1);
+	}
+	return (0);
 }
