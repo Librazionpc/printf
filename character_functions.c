2ab25d0e9c1c05dbce09a85f19b46af180d34158
PcNerd <ajayihabeeb977@gmail.com>
2023-08-19 16:01:21 +0100

    wrote a c function that handle conversion and stoers the result in a string

--- character_functions.c
+++ character_functions.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
 
 
 /**
@@ -31,4 +32,68 @@ int print_string(char *s)
 		write(1, &s[i], 1);
 	}
 	return (0);
+}
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+
+char *print_int( int num, int base, int capital)
+{
+	char *string;
+	unsigned int mul = 1;
+	int remainder = 0;
+	int lenght = 1, i = 0;
+	char character;
+	if (num < 0)
+	{
+		num = -num;
+		print_char('-');
+	}
+	if (num > base)
+	{
+		while ((num / mul) > base)
+		{
+			mul *= base;
+			lenght++;
+		}
+		string = malloc(sizeof(char) * (lenght +1));
+		string[i] = (num / mul) + '0';
+		i++;
+		while (mul >= base)
+		{
+			mul /= base;
+			remainder = (num / mul) % base;
+			if (remainder > 10)
+			{
+				if (capital == 0)
+					character = (remainder - 10) + 'a';
+				else if (capital == 1)
+					character = (remainder - 10) + 'A';
+			}
+			else
+				character = (remainder + '0');
+			string[i] = character;
+			i++;
+		}
+	}
+	else
+	{
+		string = malloc(sizeof(char) * (lenght + 1));
+		if (num > 10)
+		{
+			if (capital == 0)
+				string[0] = (num - 10) + 'a'; 
+			else if (capital == 1)
+				string[0] = (num - 10) + 'A';
+		}
+		else
+			string[0] = num + '0'; 
+	}
+	string[lenght] = '\0';
+	return (string);
 }
--- main.h
+++ main.h
@@ -2,5 +2,6 @@
 #define _MAIN_H_
 int _printf(const char *format, ...);
 int print_string(char *s);
+char *print_int(int n, int base, int cap);
 int print_char(char c);
 #endif
