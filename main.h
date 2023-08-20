73a064f54de6bae2463f1b16a2c4fb6c4d9495fd
PcNerd <ajayihabeeb977@gmail.com>
2023-08-20 17:54:25 +0100

    i pray this will resolve the merge conflic comming back

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#define BUFFER_SIZE 1024
+
+int _printf(const char *format, ...);
+char *print_string(char *s);
+char *print_int(unsigned int n, unsigned int base, int cap);
+char *print_char(char c);
+char *binary(int number);
+char *oct(int number);
+char *hex(int number,char cap);
+char *print_unsigned_int(unsigned int number);
+char *_int(int number);
+
+#endif
