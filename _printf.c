e5807c011780f5d64edce00badd4b38ed17a58f6
Librazionpc <nwajeizion393@gmail.com>
2023-08-21 10:22:10 +0100

    git

--- _printf.c
+++ _printf.c
@@ -33,7 +33,7 @@ int _printf(const char *format, ...)
 					lenght++;
 					break;
 				case 's':
-					lenght += print_string(va_arg(args, char *));
+					lenght += print_string(va_arg(args, char *)); 
 					break;
 				case '%':
 					print_char('%');
---
+++ test.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%b\n", 98);
+    _printf("%b\n", 98);
+    return (0);
+}
