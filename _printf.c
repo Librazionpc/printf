f31582bcefcd26f0a2cf50eed4a003a069845c2b
PcNerd <ajayihabeeb977@gmail.com>
2023-08-25 18:38:43 +0100

    lets see if it will work

---
+++ _printf.c
@@ -0,0 +1,92 @@
+#include "main.h"
+#include <stdio.h>
+
+int print_buffer(char *buffer, unsigned int *buffer_index, char *string);
+/**
+ * _printf - Function that print to tandard.out
+ * @format: Fetches the from va_args
+ *
+ * Return: The lenght
+ */
+
+int _printf(const char *format, ...)
+{
+	char buffer[BUFFER_SIZE], spec;
+	unsigned int buffer_index = 0;
+	int i;
+	va_list args;
+	int lenght = 0;
+	char *string;
+
+	va_start(args, format);
+	if (format == NULL || *format == '\0')
+		return (-1);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			spec = format[i];
+			if (spec == '\0')
+				break;
+			if (spec == 's' || spec == 'S')
+			{
+				string = get_string_str(va_arg(args, char *), spec);
+			}
+			else if (spec == 'c' || spec == 'u' || spec == 'i' || spec == 'd'
+					|| spec == 'o' || spec == 'x' || spec == 'X' || spec == 'b'
+					|| spec == '%')
+			{
+				string = get_string(spec, va_arg(args, int));
+			}
+			else
+				return (-1);				
+		}
+		else
+		{
+			string = print_char(format[i]);
+		}
+		if (string == NULL)
+			return (-1);
+		lenght += print_buffer(buffer, &buffer_index, string);
+	}
+	if (buffer_index > 0)
+		write(1, buffer, buffer_index);
+	va_end(args);
+	return (lenght);
+}
+
+/**
+ * print_buffer - Function that creates 1024 bytes of spaces for _printf
+ *
+ * @buffer: Fectches the macro buffer
+ * @buffer_index: The position of the array
+ * @string: The string to be calculated
+ *
+ * Return: The length of the string
+ */
+
+int print_buffer(char *buffer, unsigned int *buffer_index, char *string)
+{
+	int i;
+	int lenght = 0;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		if (*buffer_index == BUFFER_SIZE)
+		{
+			write(1, buffer, *buffer_index);
+			*buffer_index = 0;
+			buffer[*buffer_index] = string[i];
+			*buffer_index += 1;
+		}
+		else
+		{
+			buffer[*buffer_index] = string[i];
+			*buffer_index += 1;
+		}
+		lenght++;
+	}
+	free(string);
+	return (lenght);
+}
--- main.h
+++ main.h
@@ -1,9 +1,18 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
+<<<<<<< HEAD
+=======
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+>>>>>>> 29afbf4 (wrote a custom conversion specifier for my _printf function)
 #define BUFFER_SIZE 1024
 
 int _printf(const char *format, ...);
 char *print_string(char *s);
+<<<<<<< HEAD
 char *print_int(unsigned int n, unsigned int base, int cap);
 char *print_char(char c);
 char *binary(int number);
@@ -11,5 +20,16 @@ char *oct(int number);
 char *hex(int number,char cap);
 char *print_unsigned_int(unsigned int number);
 char *_int(int number);
+=======
+char *print_int(unsigned int num, unsigned int base, int _case);
+char *_int(int num);
+char *_unsigned_int(unsigned int number);
+char *oct_conversion(unsigned int number);
+char *hex_conversion(unsigned int number, char cap);
+char *binary_conversion(unsigned int number);
+char *get_string(char spec, int value);
+char *get_string_str(char *value, char spec);
+char *print_string_(char *s);
+>>>>>>> 29afbf4 (wrote a custom conversion specifier for my _printf function)
 
 #endif
